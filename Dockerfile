9726b3cf24939697ea002d0b0c4924bf71ef4a13
Kirill Enykeev <enykeev@stackstorm.com>
2017-02-15 20:07:32 +0700

    initial

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3.6.0-onbuild
+
+CMD [ "python", "./cmd.py" ]
---
+++ cmd.py
@@ -0,0 +1,34 @@
+
+import os
+
+import discord
+import asyncio
+
+token = os.environ.get('DISCORD_TOKEN', None)
+if not token:
+    raise ValueError('Token should be provided with DISCORD_TOKEN env variable')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
